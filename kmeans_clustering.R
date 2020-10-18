03ef399a1eab449acddac85015ae804a01f8c5c2
nikolaima <nikolai.maag@gmail.com>
2020-10-18 21:28:53 +0900

    Update kmeans_clustering.R

--- kmeans_clustering.R
+++ kmeans_clustering.R
@@ -23,7 +23,7 @@ docs <- tm_map(docs, removeNumbers)
 docs <- tm_map(docs, tolower)   
 docs <- tm_map(docs, removeWords, stopwords("english"))   
 docs <- tm_map(docs, removeWords, c("robot","roomba", "vacuuming","vacuum", "deebot", "ve", "'s", "don't", "doesn't", "'m", "ive", "'s", "'ve", "'m"))
-#docs <- tm_map(docs, removeWords, stpwords)
+docs <- tm_map(docs, removeWords, stpwords)
 
 #remove this words for better clusters
 #docs <- tm_map(docs, removeWords, c("year", "months", "great"))
@@ -89,21 +89,21 @@ clusplot(as.matrix(d), kfit$cluster, color=T, shade=T, labels=2, lines=0, cex = 
 
 ------------------------
 # library(cluster)   
-# d <- dist(t(dtms), method="euclidian")   # First calculate distance between words
-# fit <- hclust(d=d, method="complete")    # Also try: method="ward.D"   
-# fit
-# 
-# plot.new()
-# plot(fit, hang=-1)
-# groups <- cutree(fit, k=6)   # "k=" defines the number of clusters you are using   
-# rect.hclust(fit, k=6, border="red") # draw dendogram with red borders around the 8 clusters   
+d <- dist(t(dtms), method="euclidian")   # First calculate distance between words
+fit <- hclust(d=d, method="complete")    # Also try: method="ward.D"   
+fit
+ 
+plot.new()
+plot(fit, hang=-1)
+groups <- cutree(fit, k=9)   # "k=" defines the number of clusters you are using   
+rect.hclust(fit, k=, border="red") # draw dendogram with red borders around the 8 clusters   
 # # Try many other k's for the interpretation of your data.
-#   
-# plot.new()
-# hcd = as.dendrogram(fit)
-# op = par(mfrow = c(2, 1))
-# plot(cut(hcd, h = 100)$upper, main = "Upper tree of cut at h=100"))
-# plot(cut(hcd, h = 100)$lower[[2]], main = "Second branch of lower tree with cut at h=100")
+   
+#plot.new()
+#hcd = as.dendrogram(fit)
+#op = par(mfrow = c(2, 1))
+#plot(cut(hcd, h = 100)$upper, main = "Upper tree of cut at h=100"))
+#plot(cut(hcd, h = 100)$lower[[2]], main = "Second branch of lower tree with cut at h=100")
 
 
 ---------------------------
@@ -111,7 +111,6 @@ clusplot(as.matrix(d), kfit$cluster, color=T, shade=T, labels=2, lines=0, cex = 
 ### 3.5  Term Correlations (Co-occurence)
   # See the description above for more guidance with correlations.
   # If words always appear together, then correlation=1.0.    
-findAssocs(dtms, "months", corlimit=0.1) # specifying a correlation limit of 0.01
-findAssocs(dtms, "year", corlimit=0.1) # specifying a correlation limit of 0.01
-findAssocs(dtms, "charging", corlimit=0.01) # specifying a correlation limit of 0.01
-##########
+findAssocs(dtms, "stuck", corlimit=0.01) # specifying a correlation limit of 0.01
+findAssocs(dtms, "stopped", corlimit=0.1) # specifying a correlation limit of 0.01
+findAssocs(dtms, "clean", corlimit=0.01) # specifying a correlation limit of 0.01
